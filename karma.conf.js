cf20ac2060473cef9e2bd9f2a95b28d07976d56b
gnxcode <genixxavier@gmail.com>
2022-05-15 00:44:08 -0500

    add gitactions

--- karma.conf.js
+++ karma.conf.js
@@ -34,10 +34,10 @@ module.exports = function (config) {
       ],
       check: {
         global: {
-          statements: 90,
-          branches: 90,
-          functions: 90,
-          lines: 90
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
         }
       }
     },
